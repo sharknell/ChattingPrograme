07fb3153c7578e9de9eb8a37a1a0e86342bd60b0
9817kg <137484097+9817kg@users.noreply.github.com>
2023-07-20 00:02:40 +0900

    Update ChatClient.java

--- src/jframe/main/ChatClient.java
+++ src/jframe/main/ChatClient.java
@@ -1,6 +1,7 @@
 package jframe.main;
 
 import java.awt.Color;
+import java.awt.Dimension;
 
 import javax.swing.DefaultListModel;
 import javax.swing.ImageIcon;
@@ -42,6 +43,11 @@ public class ChatClient extends JFrame {
 	private JLabel lblNewLabel_1;
 
 	public ChatClient() throws IOException {
+		p = new JPanel();
+		ta = new JTextArea();
+		li_inwon = new JList<>(defaultListModel);
+		
+				
 		setForeground(new Color(64, 128, 128));
 		setTitle("채팅방");
 
@@ -49,25 +55,24 @@ public class ChatClient extends JFrame {
 
 		sp_ta = new JScrollPane();
 		sp_list = new JScrollPane();
-		bt_exit = new JButton("나가기");
+		
+		bt_exit = new JButton("");
 		bt_exit.setFont(new Font("777별나라달님", Font.PLAIN, 17));
 		bt_exit.setBackground(new Color(255, 255, 255));
-
 		bt_exit.setLayout(null);
-		
 		bt_exit.setBorderPainted(false);
+		bt_exit.setIcon(new ImageIcon("image/door.png"));
 		
-		p = new JPanel();
-		bt_exit.setIcon(null);
-		sp_ta.setBounds(12, 6, 183, 390);
-		sendTF.setBounds(0, 420, 203, 41);
+		
+		sp_ta.setBounds(12, 6, 171, 390);
+		sendTF.setBounds(0, 431, 228, 30);
 
 		sp_list.setBounds(207, 3, 74, 183);
-		bt_exit.setBounds(207, 423, 74, 30);
+		bt_exit.setBounds(236, 420, 45, 41);
 		p.setLayout(null);
-		p.setBackground(new Color(64, 128, 128));
+		p.setBackground(new Color(255, 255, 255));
 		p.add(sp_ta);
-		ta = new JTextArea();
+		
 		ta.setFont(new Font("나눔고딕 ExtraBold", Font.BOLD, 12));
 		sp_ta.setViewportView(ta);
 		ta.setLineWrap(true);
@@ -75,29 +80,33 @@ public class ChatClient extends JFrame {
 		p.add(sp_list);
 
 		JLabel lblNewLabel = new JLabel("대화인원");
-		lblNewLabel.setForeground(new Color(0, 0, 128));
-		lblNewLabel.setFont(new Font("나눔손글씨 펜", Font.PLAIN, 17));
+		lblNewLabel.setOpaque(true);
+		lblNewLabel.setForeground(new Color(0, 0, 0));
+		lblNewLabel.setFont(new Font("HY엽서M", Font.PLAIN, 11));
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setBackground(new Color(255, 255, 255));
 		sp_list.setColumnHeaderView(lblNewLabel);
 		
-		li_inwon = new JList<>(defaultListModel);
+		
 		sp_list.setViewportView(li_inwon);
 		p.add(bt_exit);
 
 		getContentPane().add(p);
 		
-		bt_perChat = new JButton("귓속말");
+		bt_perChat = new JButton("");
+		bt_perChat.setOpaque(false);
+		bt_perChat.setIcon(new ImageIcon("image/wishper.png"));
+		bt_perChat.setPreferredSize(new Dimension(30, 30));
 		bt_perChat.setFont(new Font("777별나라달님", Font.PLAIN, 17));
 		bt_perChat.setBorderPainted(false);
 		bt_perChat.setBackground(Color.WHITE);
-		bt_perChat.setBounds(207, 391, 74, 30);
+		bt_perChat.setBounds(251, 196, 30, 30);
 		p.add(bt_perChat);
 		
 		lblNewLabel_1 = new JLabel("Message");
-		lblNewLabel_1.setForeground(new Color(255, 255, 255));
+		lblNewLabel_1.setForeground(new Color(0, 0, 0));
 		lblNewLabel_1.setFont(new Font("Gadugi", Font.ITALIC, 13));
-		lblNewLabel_1.setBounds(12, 397, 74, 21);
+		lblNewLabel_1.setBounds(12, 406, 74, 21);
 		p.add(lblNewLabel_1);
 		setBounds(300, 200, 302, 500);
 		sendTF.requestFocus();
