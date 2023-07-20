6b68ebb064636d4b5e13da32b73912e5aa9938ac
FoongGom <137484083+FoongGom@users.noreply.github.com>
2023-07-20 11:22:12 +0900

    가입할때 인증방법 수정
    인증하기 버튼만들어서 새로운 클래스 호출 후 인증방법선택

--- src/jframe/menu/JoinScreen.java
+++ src/jframe/menu/JoinScreen.java
@@ -39,7 +39,6 @@ public class JoinScreen extends JFrame {
     private JTextField textField_3;
     private JTextField textField_4;
     private JPasswordField passwordField;
-    private JTextField textFieldVerification;
     
 
     private boolean isEmailSent = false; // 이메일 발송 여부
@@ -117,12 +116,6 @@ public class JoinScreen extends JFrame {
         lblNewLabel_4.setFont(new Font("휴먼둥근헤드라인", Font.PLAIN, 16));
         lblNewLabel_4.setBounds(49, 310, 68, 20);
         contentPane.add(lblNewLabel_4);
-
-        JLabel lblNewLabel_5 = new JLabel("이메일인증");
-        lblNewLabel_5.setForeground(new Color(0, 0, 0));
-        lblNewLabel_5.setFont(new Font("휴먼둥근헤드라인", Font.PLAIN, 16));
-        lblNewLabel_5.setBounds(48, 340, 80, 20);
-        contentPane.add(lblNewLabel_5);
 
         textField = new JTextField();
         textField.setForeground(new Color(255, 255, 255));
@@ -252,7 +245,7 @@ public class JoinScreen extends JFrame {
                 String name = textField_2.getText();
                 String phoneNumber = textField_3.getText();
                 String RRN = textField_4.getText();
-                String verification = textFieldVerification.getText();
+
 
                 List<String> errorMessages = new ArrayList<>();
 
@@ -271,13 +264,6 @@ public class JoinScreen extends JFrame {
                 if (RRN.isEmpty()) {
                     errorMessages.add("IDNum");
                 }
-                if (!isEmailSent) {
-                    errorMessages.add("이메일 인증을 진행해주세요.");
-                }
-                if (!verification.equals(verificationCode)) {
-                    errorMessages.add("올바른 인증 코드를 입력해주세요.");
-                }
-
                 if (!errorMessages.isEmpty()) {
                     StringBuilder errorMessage = new StringBuilder("다음 항목이 입력되지 않았거나 올바르지 않습니다:\n");
                     for (String error : errorMessages) {
@@ -291,7 +277,6 @@ public class JoinScreen extends JFrame {
                     textField_2.setText("");
                     textField_3.setText("");
                     textField_4.setText("");
-                    textFieldVerification.setText("");
 
                     // 선택된 필드로 포커스 이동
                     textField.requestFocusInWindow();
@@ -324,6 +309,23 @@ public class JoinScreen extends JFrame {
         JLabel lblImage_2 = new JLabel(imageIcon_2);
         lblImage_2.setBounds(106, 270, 38, 20);
         panel_2.add(lblImage_2);
+        
+        JPanel panel_3 = new JPanel();
+        panel_3.addMouseListener(new MouseAdapter() {
+        	@Override
+        	public void mouseClicked(MouseEvent e) {
+        		Authenticate atc = new Authenticate();
+        		atc.setVisible(true);
+        	}
+        });
+        panel_3.setBackground(new Color(185, 207, 210));
+        panel_3.setBounds(189, 330, 70, 25);
+        contentPane.add(panel_3);
+        
+        ImageIcon imageIcon_3 = new ImageIcon("image/authenticate.png");
+        JLabel lblImage_3 = new JLabel(imageIcon_3);
+        lblImage_3.setBounds(106, 270, 38, 20);
+        panel_3.add(lblImage_3);
 
         panel_2.addMouseListener(new MouseAdapter() {
             @Override
@@ -333,79 +335,6 @@ public class JoinScreen extends JFrame {
                 JoinScreen.this.dispose();
             }
         });
-
-        final JPanel panel_3 = new JPanel();
-        panel_3.setBackground(new Color(185, 207, 210));
-        panel_3.setBounds(189, 371, 70, 25);
-        contentPane.add(panel_3);
-
-        ImageIcon imageIcon_3 = new ImageIcon("image/email.png");
-        JLabel lblImage_3 = new JLabel(imageIcon_3);
-        lblImage_3.setBounds(106, 270, 38, 20);
-        panel_3.add(lblImage_3);
-
-        final JPanel panel_4 = new JPanel();
-        panel_4.setBackground(new Color(185, 207, 210));
-        panel_4.setBounds(189, 361, 70, 25);
-        contentPane.add(panel_4);
-        panel_4.setLayout(null);
-        panel_4.setVisible(false);
-
-        panel_4.setLayout(new BorderLayout());
-        ImageIcon imageIcon_4 = new ImageIcon("image/email_success.png");
-        JLabel lblImage_4 = new JLabel(imageIcon_4);
-        lblImage_4.setBounds(106, 270, 38, 20);
-        panel_4.add(lblImage_4, BorderLayout.CENTER);
-
-        panel_3.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                if (!isEmailSent) {
-                    String id = textField.getText();
-                    if (SignUp.isValidEmail(id)) {
-                        // 이메일 발송 코드 작성
-                        EmailSender emailSender = new EmailSender();
-                        verificationCode = generateVerificationCode();
-                        emailSender.sendVerificationEmail(id, verificationCode);
-                        JOptionPane.showMessageDialog(JoinScreen.this, "인증 코드가 이메일로 전송되었습니다.");
-
-                        panel_3.setVisible(false);
-                        panel_4.setVisible(true);
-                        isEmailSent = true;
-                    } else {
-                        JOptionPane.showMessageDialog(JoinScreen.this, "유효한 이메일 형식이 아닙니다.");
-                    }
-                }
-            }
-        });
-        
-        panel_4.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                String enteredCode = textFieldVerification.getText();
-                String generatedCode = generateVerificationCode();
-
-                String lastSixDigitsGenerated = generatedCode.substring(generatedCode.length() - 6);
-                if (enteredCode.equals(lastSixDigitsGenerated)) {
-                    JOptionPane.showMessageDialog(JoinScreen.this, "인증되었습니다.");
-                    panel_4.setVisible(false);
-                } else {
-                    JOptionPane.showMessageDialog(JoinScreen.this, "인증되지 않았습니다.");
-                    return;
-                }
-            }
-        });
-
-
-
-        
-        textFieldVerification = new JTextField();
-        textFieldVerification.setForeground(new Color(255, 255, 255));
-        textFieldVerification.setBackground(new Color(155, 174, 176));
-        textFieldVerification.setColumns(10);
-        textFieldVerification.setBounds(129, 342, 130, 20);
-        textFieldVerification.setBorder(BorderFactory.createEmptyBorder());
-        contentPane.add(textFieldVerification);
     }
 
     private String generateVerificationCode() {
@@ -419,5 +348,4 @@ public class JoinScreen extends JFrame {
         }
         return verificationCode;
     }
-
 }
